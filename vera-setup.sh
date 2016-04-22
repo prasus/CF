d1c44391bc630fa20de9375bf87c27a3c10c7b45
prasus <prasus@gmail.com>
2016-04-22 13:07:10 +0530

    updated vera-env.sh

--- vera-setup.sh
+++ vera-setup.sh
@@ -18,7 +18,7 @@ yum remove  -y java-1.8.0-openjdk
 yum install -y java-1.7.0-openjdk
 
 echo "veraIP=$PUBLIC_IP" > ~vera/.veraIP
-echo "VERA_ENV=$VERA_ENV" > /etc/profile.d/vera_env.sh
+echo "export VERA_ENV=$VERA_ENV" > /etc/profile.d/vera_env.sh
 
 curl -Is http://www.vera.com | head -1
 
